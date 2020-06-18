b657a5ce606a3d378d9a076992b8b4a175a720b6
Jawad Raza <razajawad840@gmail.com>
2020-06-18 15:09:43 +0500

    domain changed

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(abc.curriki.org)
+domains=(devstudio.curriki.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="razajawad840@gmail.com" # Adding a valid address is strongly recommended
