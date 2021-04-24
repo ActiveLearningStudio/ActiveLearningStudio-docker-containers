54cdb791f27ff3ed768a595bbe54506e3e3082b2
Ali Mehdi <imcoder001@gmail.com>
2021-04-24 12:06:08 +0000

    updated

--- tsugi/tsugi-main-config.example.php
+++ tsugi/tsugi-main-config.example.php
@@ -117,7 +117,7 @@ $CFG->dbprefix  = '';
 // or a sha256 hash of the admin password.  Please don't use either
 // the 'tsugi' or the sha256 of 'tsugi' example values below.
 // $CFG->adminpw = false;
-$CFG->adminpw = 'substitute-admin-password';
+$CFG->adminpw = 'substitute-tsugi-admin-password';
 // $CFG->adminpw = 'sha256:9c0ccb0d53dd71b896cde69c78cf977acbcb36546c96bedec1619406145b5e9e';
 
 // Theme Tsugi to your institutions colors. If not set, default colors will be used.
