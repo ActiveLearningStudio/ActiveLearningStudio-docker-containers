fc4e492ca7564ba763e1ac871755853a2f6ffd79
Ali Mehdi <imcoder001@gmail.com>
2021-04-25 01:11:35 +0000

    update

---
+++ api/storage/storage/app/public/h5p/libraries/H5PEditor.GeoGebra3DWidget-1.0/scripts/widget.js
@@ -0,0 +1,170 @@
+var H5PEditor = H5PEditor || {};
+/**
+ * Applet Editor widget module
+ *
+ * @param {H5P.jQuery} $
+ */
+H5PEditor.widgets.GeoGebra3DWidget = H5PEditor.GeoGebra3DWidget = (function ($) {
+
+  /**
+   * Creates color selector.
+   *
+   * @class H5PEditor.GeoGebra3DWidget
+   *
+   * @param {Object} parent
+   * @param {Object} field
+   * @param {string} params
+   * @param {H5PEditor.SetParameters} setValue
+   */
+  function C(parent, field, params, setValue) {
+    this.parent = parent;
+    this.field = field;
+    this.params = params;
+    this.setValue = setValue;
+    this.uniq_id = Date.now() + Math.floor(Math.random() * 1000);
+    this.ggb_element = "ggb-element-" + this.uniq_id;
+    this.ggb_applet = "ggbApplet_" + this.uniq_id;
+    this.ggbApp = [];
+  }
+
+  /**
+   * Append the field to the wrapper.
+   *
+   * @param {H5P.jQuery} $wrapper
+   */
+  C.prototype.appendTo = function ($wrapper) {
+    var self = this;
+
+    self.$container = $('<div>', {
+      'class': 'field text h5p-classic-applet'
+    });
+
+    // Add header:
+    $('<span>', {
+      'class': 'h5peditor-label',
+      html: self.field.label
+    }).appendTo(self.$container);
+
+    // Add description:
+    $('<span>', {
+      'class': 'h5peditor-field-description',
+      'id': self.ggb_element,
+      html: self.field.description
+    }).appendTo(self.$container);
+
+    self.$container.appendTo($wrapper);
+    self.initializeGeoGabra();
+  };
+
+  /**
+   * Create GeoGebra Applet
+   */
+  C.prototype.initializeGeoGabra = function () {
+    this.ggbApp = new GGBApplet({
+      "appName": "3d",
+      "width": 800,
+      "height": 600,
+      "showToolBar": true,
+      "showAlgebraInput": true,
+      "showMenuBar": false,
+      "id": this.ggb_applet,
+      "useBrowserForJS": false,
+    }, true );
+
+    this.activityData().then(value => {
+      if (this.params){
+        this.loadActivityData();
+      }
+      this.setActivityData();
+    });
+  }
+
+  /**
+   * Set and load activity data
+   */
+  C.prototype.activityData = async function () {
+    let time = 0;
+
+    // wait until ggb applet element available
+    while (! document.getElementById(this.ggb_element)) {
+      await this.delay(50);
+    }
+
+    // inject the applet data
+    this.reInjectApplet();
+
+    // wait until ggb applet becomes available
+    while (!window[this.ggb_applet] || typeof window[this.ggb_applet].setBase64 !== "function") {
+      await this.delay(500);
+      time += 500;
+      if (time > 3000) {
+        this.reInjectApplet();
+        time = 0;
+      }
+    }
+  }
+
+  /**
+   * Load activity data from base64 string
+   */
+  C.prototype.loadActivityData = function () {
+    window[this.ggb_applet].setBase64(this.params);
+  }
+
+  /**
+   * Set activity data from base64 string to field
+   */
+  C.prototype.setActivityData = function () {
+    const that = this;
+    setInterval(function () {
+      that.setWidget(window[that.ggb_applet].getBase64());
+    }, 500);
+  }
+
+  /**
+   * Re-Inject the applet into ggb-element
+   */
+  C.prototype.reInjectApplet = function () {
+    if (!$("#" + this.ggb_element).find("article div").length) {
+      this.ggbApp.inject(this.ggb_element);
+    }
+  }
+
+  /**
+   * Set widget base64 encoded value
+   * @param val
+   */
+  C.prototype.setWidget = function (val) {
+    // this.params = val;
+    this.setValue(this.field, val);
+  };
+
+  /**
+   * Validate the current values.
+   *
+   * @returns {boolean}
+   */
+  C.prototype.validate = function () {
+  };
+
+  /**
+   * Remove the current field
+   */
+  C.prototype.remove = function () {
+  };
+
+  /**
+   * Create delay for ms
+   * @param ms
+   * @returns {Promise<unknown>}
+   */
+  C.prototype.delay = function (ms) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(2);
+      }, ms);
+    });
+  }
+
+  return C;
+})(H5P.jQuery);
---
+++ api/storage/storage/app/public/h5p/libraries/XH5P.Geometry-1.0/geogabra.js
@@ -0,0 +1,36 @@
+var H5P = H5P || {};
+ 
+H5P.Geometry = (function ($) {
+  /**
+   * Constructor function.
+   */
+  function C(options, id) {
+    // Extend defaults with provided options
+    this.options = $.extend(true, {}, {
+      greeting: 'Hello world!',
+      image: null
+    }, options);
+    // Keep provided id.
+    this.id = id;
+  };
+ 
+  /**
+   * Attach function called by H5P framework to insert H5P content into
+   * page
+   *
+   * @param {jQuery} $container
+   */
+  C.prototype.attach = function ($container) {
+    // Set class on container to identify it as a greeting card
+    // container.  Allows for styling later.
+    //$container.addClass("h5p-greetingcard");
+	$container.attr("id","ggb-element");
+    
+	var ggbApp = new GGBApplet({"appName": "geometry", "width": 800, "height": 600, "showToolBar": true, "showAlgebraInput": true, "showMenuBar": true }, true);
+    window.addEventListener("load", function() { 
+        ggbApp.inject('ggb-element');
+    });
+  };
+ 
+  return C;
+})(H5P.jQuery);
